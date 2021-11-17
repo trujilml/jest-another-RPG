03218ab8bd11f61554e30e6b01affc00a98e5088
trujilml <mickeytrj@gmail.com>
2021-11-17 16:38:40 -0600

    added test folder for game objects and started potion function to run in jest

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,9 @@
+
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
